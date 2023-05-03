03fd2ebd6ffb39e95b1b9c7182a9dd18a34840c1
GatoSkate <129997638+GatoSkate@users.noreply.github.com>
2023-05-03 16:12:28 -0400

    Plantilla de trabajo
    trabajen xdnt

--- Proyecto/Proyecto/urls.py
+++ Proyecto/Proyecto/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import mostrar_principal
 
 urlpatterns = [
+    path('', mostrar_principal, name="pagina_principal"),
     path('admin/', admin.site.urls),
 ]
--- Proyecto/Proyecto/views.py
+++ Proyecto/Proyecto/views.py
@@ -1,1 +1,8 @@
-import 
+from django.shortcuts import render
+
+
+def mostrar_principal(request):
+    return render(request, 'principal.html')
+
+def mostrar_sesion(request):
+    return render(request, 'sesion.html')
