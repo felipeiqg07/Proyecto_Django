3037226fbaf8bc4ce554626a79306ec9fcabd70c
GatoSkate <ric.flores@duocuc.cl>
2023-05-04 22:41:16 -0400

    Cambios de la principal y otros
    cambio de principal, productos y views etc... Arriba españa joer!

--- Proyecto/Proyecto/views.py
+++ Proyecto/Proyecto/views.py
@@ -1,3 +1,4 @@
+
 from django.shortcuts import render
 from .productos import productos
 
@@ -8,8 +9,22 @@ def mostrar_sesion(request):
     return render(request, 'sesion.html')
 
 def mostrar_productos(request):
+    resultado = productos
+    palabra = ""
+    if request.method == 'GET':
+        palabra_cliente= request.GET.get('busqueda')
+        if palabra_cliente != None:
+            palabra = palabra_cliente
+            resultado = []
+            for variable in productos:
+                if palabra_cliente.lower() in variable['nombre'].lower() or palabra_cliente.lower() in variable['detalle'].lower():
+                    resultado.append(variable)
+            # Logica de busqueda
+            pass
+    print(request.method)
     context = {
         'titulo':'productitos epicos',
-        'lista': productos,
+        'lista': resultado,
+        'palabra': palabra
     }
     return render(request, 'productos.html', context)
